38a495cff569b5a9f8ef3ca1298a9173d2cb17a9
Nicolas Danelon <nicolasdanelon@gmail.com>
2022-12-12 14:44:37 -0300

    crud is done. update and remove usrs is ready to rock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
-from tkinter import ttk, messagebox
+from tkinter import ttk
+from tkinter.messagebox import askyesno
 from user import User
 from lib import isValid
 import db, traceback, datetime
@@ -16,7 +17,6 @@ class App(customtkinter.CTk):
         # configure window
         self.title("User admin interface demo")
         self.geometry(f"{1100}x{580}")
-        self.edit_id_field_value=""
 
         # configure grid layout (4x4)
         self.grid_columnconfigure(1, weight=1)
@@ -97,6 +97,40 @@ class App(customtkinter.CTk):
             self.name_field.delete(0, tk.END)
             self.name_field.insert(0, "")
 
+    def update_user_action(self):
+        usr = User(
+            id = self.edit_id_field.cget("text"),
+            email = self.edit_email_field.get(),
+            name = self.edit_name_field.get(),
+            created_at = datetime.datetime.now()
+        )
+
+        if (usr.id != "" and usr.name != "" and isValid(usr.email)):
+            db.update(usr)
+            self.select_frame_by_name("table")
+            self.print_table()
+        else:
+            print("Error")
+
+    def delete_user_action(self):
+        id = self.edit_id_field.cget("text")
+        if id is not None:
+            db.delete(id)
+            self.select_frame_by_name("table")
+            self.print_table()
+        else:
+            print("error")
+
+    def confirm_delete(self):
+        answer = askyesno(title='Remove user confirmation', message='Are you sure that you want to remove the user?')
+        if answer:
+            self.delete_user_action()
+
+    def confirm_update(self):
+        answer = askyesno(title='Update user onfirmation', message='Are you sure that you want to update the user?')
+        if answer:
+            self.update_user_action()
+
     def print_edit_form(self):
         self.edit_form = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
 
@@ -112,42 +146,36 @@ class App(customtkinter.CTk):
 
         self.edit_id = customtkinter.CTkLabel(self.edit_form, text="ID")
         self.edit_id.grid(row=1, column=0)
-
-        self.edit_id_field = customtkinter.CTkLabel(self.edit_form, text=self.edit_id_field_value, anchor="w")
+        self.edit_id_field = customtkinter.CTkLabel(self.edit_form, text="", anchor="w")
         self.edit_id_field.grid(row=1, column=1, ipadx=135, pady=5)
-
-        # self.edit_id_field = customtkinter.CTkEntry(self.edit_form, placeholder_text=self.edit_id_field_value)
-        # self.edit_id_field.grid(row=1, column=1, ipadx=100, pady=5)
 
         self.go_back_button = customtkinter.CTkButton(self.edit_form, command=self.table_button_event, text="Cancel")
         self.go_back_button.grid(row=4, column=0, padx=5, pady=10)
 
-        self.edit_submit_button = customtkinter.CTkButton(self.edit_form, command=self.update_user_form, text="Update user")
+        self.edit_submit_button = customtkinter.CTkButton(self.edit_form, command=self.confirm_update, text="Update user")
         self.edit_submit_button.grid(row=4, column=1, padx=20, pady=10)
+
+        self.delete_button = customtkinter.CTkButton(self.edit_form, command=self.confirm_delete, text="Delete user",
+                                                    fg_color=("#aa0000", "#bb0000"),
+                                                    text_color=("gray10", "gray90"),
+                                                    hover_color=("#990000", "#990000"))
+        self.delete_button.grid(row=4, column=2, padx=20, pady=10)
 
     def double_click_table_item(self, event):
         item = self.table.selection()
         self.print_edit_form()
         self.edit_name_field.delete(0, tk.END)
         self.edit_email_field.delete(0, tk.END)
-        # self.edit_id_field.delete(0, tk.END)
 
         for i in item:
             self.edit_name_field.insert(0, self.table.item(i, "values")[2])
             self.edit_email_field.insert(0, self.table.item(i, "values")[1])
-            # self.edit_id_field.insert(0, self.table.item(i, "values")[0])
             self.edit_id_field.configure(text = self.table.item(i, "values")[0])
-
-            # print(self.table.item(i, "values")[0])
-            # print(self.edit_id_field_value)
 
         self.select_frame_by_name("edit_form")
         sleep(1)
         self.edit_name.focus_set()
         self.edit_email_field.focus_set()
-
-    def update_user_form(self):
-        print(1)
 
     def change_scaling_event(self, new_scaling: str):
         new_scaling_float = int(new_scaling.replace("%", "")) / 100
